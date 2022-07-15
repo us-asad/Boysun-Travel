534760391b6923b60a59ac182b7478270b78a2eb
Asad <usasad99@gmail.com>
2022-07-15 14:09:12 +0500

    project completed

--- assets/js/home.js
+++ assets/js/home.js
@@ -47,10 +47,31 @@ const handleAboutVideoPlay = () => {
   })
 }
 
+const handleLoadMore = (btn, moreData) => {
+  btn.addEventListener("click", () => {
+    if (moreData.classList.contains("!hidden")) {
+      moreData.classList.replace("!hidden", "grid");
+      btn.innerText = "Load Less";
+    } else {
+      moreData.classList.replace("grid", "!hidden");
+      btn.innerText = "Load More";
+    }
+  })
+}
 
+const handleLoadMoreData = () => {
+  const popularPlacesBtn = $("#load-more-popular-places");
+  const morePopularPlaces = $("#more-popular-places");
+  const bestHotelsBtn = $("#load-more-best-hotels");
+  const moreBestHotels = $("#more-best-hotels");
+
+  handleLoadMore(popularPlacesBtn, morePopularPlaces);
+  handleLoadMore(bestHotelsBtn, moreBestHotels);
+}
 
 window.addEventListener("DOMContentLoaded", () => {
   handleMainSlider();
   handleAboutVideoPlay();
+  handleLoadMoreData();
 });
 
